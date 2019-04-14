e62a485777f1fb47615c2911ff9577d39882b612
dannyadu <danny_adu@hotmail.com>
2019-04-14 18:01:57 +0200

    Allow cross origin requests

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const router = express.Router()
 
 const api_controller = require(path.join("..", "controllers", "apiController"));
 
-router.get('/locations', api_controller.locations)
+router.get('/locations', cors(), api_controller.locations)
 
 module.exports = router;
